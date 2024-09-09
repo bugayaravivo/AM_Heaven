c595cb5a96bd56d40786f35966ab9b6db407c784
EC2 Default User <ec2-user@ip-172-31-14-221.ap-northeast-1.compute.internal>
2024-09-09 03:07:29 +0000

    [finish]post.new,create

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,8 +1,20 @@
 class Public::PostsController < ApplicationController
   def new
+    @post = Post.new
   end
+  
+  def create
+    @post = current_user.posts.new(post_params)
+    
+    if @post.save
+      redirect_to post_path(@post.id)
+    else
+      render :new
+    end 
+  end 
 
   def index
+    @posts = Post.all
   end
 
   def show
@@ -10,4 +22,16 @@ class Public::PostsController < ApplicationController
 
   def edit
   end
+  
+  def update
+  end 
+  
+  def destroy
+  end 
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body, :address, :user_id)
+  end 
 end
