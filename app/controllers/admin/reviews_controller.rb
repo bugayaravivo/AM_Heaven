19b64fd0bf9fb83f22d77f5522040d3dacbc5db4
EC2 Default User <ec2-user@ip-172-31-14-221.ap-northeast-1.compute.internal>
2024-09-24 02:36:39 +0000

    [fix]work_show

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,10 +1,18 @@
 class Admin::ReviewsController < ApplicationController
   before_action :authenticate_admin!
   
-  
   def index
   end
 
   def show
   end
+  
+  def destroy
+  end 
+  
+  private
+  
+  def review_params
+    params.require(:review).permit(:title, :body, :rating, :work_id, :spot_id)
+  end 
 end
