f1c0cbd3639370ce9b9f10e98fac0fd9270096e6
EC2 Default User <ec2-user@ip-172-31-14-221.ap-northeast-1.compute.internal>
2024-09-27 09:36:12 +0000

    [fix]homes_controller

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
-    @top_posts = Post.order("RANDOM()").limit(3)
-    @top_spots = Spot.order("RANDOM()").limit(10)
+    @top_posts = Post.order("RAND()").limit(3)
+    @top_spots = Spot.order("RAND()").limit(10)
   end
 
   def about
