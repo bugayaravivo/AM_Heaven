90518ffb56079262294931680bdc3d4775647a50
EC2 Default User <ec2-user@ip-172-31-14-221.ap-northeast-1.compute.internal>
2024-10-05 10:42:16 +0000

    [fix]コメントに感情分析スコア、管理者側で投稿削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_02_101759) do
+ActiveRecord::Schema.define(version: 2024_10_05_043218) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2024_10_02_101759) do
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "post_spots", force: :cascade do |t|
